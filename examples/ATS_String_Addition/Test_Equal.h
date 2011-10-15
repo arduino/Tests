cf547fc0e8807d36e97f7b2fc1d59e11cba62647
Ricklon <rick.rickanderson@gmail.com>
2011-10-15 16:01:17 -0400

    Missing tests from Paul Stoffrogen added.

--- examples/ATS_String_Addition/Test_Equal.h
+++ examples/ATS_String_Addition/Test_Equal.h
@@ -1,13 +1,16 @@
-void Test_Equal(char *testString, char *expected, const String &actual)
-{
-  char buf[100]; actual.toCharArray(buf, 100);
-  boolean b = (strcmp(buf, expected) == 0);
-  ATS_PrintTestStatus(testString, b);
-  if (!b) {
-    Serial.print("expected '");
-    Serial.print(expected);
-    Serial.print("', actual '");
-    Serial.print(actual);
-    Serial.println("'");
-  }
-}
+#if ARDUINO >= 100
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#if ARDUINO >= 100 || defined(CORE_TEENSY)
+void Test_Equal(char *testString, char *expected, const String &actual);
+#else
+// the old string code does not define toCharArray() as const
+// this simple but ugly workaround is for Arduino 0022 and
+// should someday be removed.  For more info, see
+// http://code.google.com/p/arduino/issues/detail?id=552
+void Test_Equal(char *testString, char *expected, String actual);
+#endif
+
